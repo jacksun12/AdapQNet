cfcc1fff3142d315529260dd016ea405fe0f4ead
YSUN012 <112078893+jacksun12@users.noreply.github.com>
2025-07-30 19:31:15 +0800

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,4 @@
 from .mobilenetv2 import AdaptQMobileNetV2
-from .mobilenetv3 import AdaptQMobileNetV3
-from .efficientnet import AdaptQEfficientNet
 from .base import BaseModel, MixedPrecisionLayer
 
 __all__ = [
